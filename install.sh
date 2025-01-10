b8ee187499d4890fb177665b4200dda72591556a
lh0n <1873921+lh0n@users.noreply.github.com>
2025-01-10 23:08:22 +0100

    feat(install): initial checkin for install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Script for automating the initial setup and maintenance of dotfiles.
+#
+# Goals:
+#   1) Idempotency. It should always produce the same result, regardless of
+#      how many times this script is executed.
+#   2) Dependencies. It assumes dependencies are already installed via ansible.
+#      Therefore, it won't validade whether dependencies already exist.
+
+set -o xtrace   # verbose
+set -o errexit  # stop on error
+
+echo "[STOW]: Main Package"
+stow --verbose --restow --adopt .
+echo
+
+echo "[STOW]: Outdoor (/etc) Package"
+pushd outdoor
+sudo stow --verbose --restow --adopt --target /etc etc
+popd
+echo
+
+echo "[STOW]: outdoor (/usr) Package"
+pushd outdoor
+sudo stow --verbose --restow --adopt --target /usr usr
+popd
+echo
